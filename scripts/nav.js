699cb521167deeb85a5bb0f0511a3e1a9943615c
andrzejszl <andrzej.sz123@gmail.com>
2022-07-11 19:02:01 +0200

    add nav for mobile

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,4 +1,11 @@
-$('nav a').on('click', function () {
+$('nav.desktop a').on('click', function () {
+    const $goToSection = "#" + $(this).attr('class');
+    $('body, html').animate({
+        scrollTop: ($($goToSection).offset().top - 70)
+    })
+})
+
+$('nav.switch a').on('click', function () {
     const $goToSection = "#" + $(this).attr('class');
     $('body, html').animate({
         scrollTop: $($goToSection).offset().top
